d5c427712a59b8d3082bfaba83b9dd71f31aaad7
Marqui <threeee@skiff.com>
2024-02-04 20:37:51 -0800

    Update datetime.php

---
+++ datetime.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set("America/Chicago"); // Set the timezone to Central Time
+
+$currentDate = date('l, F j, Y'); // Format the date
+
+echo $currentDate; // Output the date
+?>
